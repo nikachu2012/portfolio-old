8a9d572b8c8901cba282d167776539c096dde670
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2023-02-23 01:49:03 +0900

    [update] MenuBar.tsxを移動

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const MenuBar = () => {
+    return (
+        <>
+            <li><Link to={`/profile`}>👤Profile</Link></li>
+            <li><Link to={`/skill`}>🎯Skill</Link></li>
+            <li><Link to={`/timeline`}>📅Timeline</Link></li>
+            <li><Link to={`/works`}>👨‍💻Works</Link></li>
+            <li><Link to={`/gadget`}>💻Gadget</Link></li>
+            <li><Link to={`/social`}>🌐Social</Link></li>
+        </>
+    )
+}
+
+export default MenuBar
