7ca1ff4510b0c616fbbc979b2a99a4412156ba1b
nikachu <75615396+nikachu2012@users.noreply.github.com>
2023-11-09 12:47:07 +0900

    [fix] TwitterをXに変更

--- src/Social.tsx
+++ src/Social.tsx
@@ -38,7 +38,7 @@ function Social() {
                     <div className="w-full h-[calc(100vh_-_64px)] flex justify-center items-center flex-col">
                         <h2 className="text-2xl font-bold">nikachuのSNS</h2>
                         <div className="flex flex-col items-center [&>*]:my-[5px] md:items-normal md:flex-row md:mt-[10px] md:[&>*]:mx-[5px]">
-                            <a href="https://twitter.com/nikachu2012" target={"_blank"}><button className="btn btn-primary">Twitter</button></a>
+                            <a href="https://twitter.com/nikachu2012" target={"_blank"}><button className="btn btn-primary">X(Twitter)</button></a>
                             <a href="https://github.com/nikachu2012" target={"_blank"}><button className="btn btn-primary">GitHub</button></a>
                             <a href="https://www.instagram.com/nikachu2012/" target={"_blank"}><button className="btn btn-primary">Instagram</button></a>
                             <a href="https://www.youtube.com/@nikachu2012" target={"_blank"}><button className="btn btn-primary">YouTube</button></a>
